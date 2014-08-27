bf722f13ac20e36c7f1e17c38446dee438300e73
jbauschatz <john.bauschatz@my.wheaton.edu>
2014-08-27 09:27:39 -0500

    Merged server api project in

---
+++ src/main/java/com/conchordance/api/ConchordanceAPI.java
@@ -0,0 +1,44 @@
+package main.java.com.conchordance.api;
+
+import com.conchordance.Conchordance;
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import main.java.com.conchordance.api.resources.ChordResource;
+import main.java.com.conchordance.api.resources.FretboardResource;
+import main.java.com.conchordance.api.resources.InstrumentResource;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
+
+public class ConchordanceAPI extends Application<Configuration> {
+    public static void main(String[] args) throws Exception {
+        new ConchordanceAPI().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<Configuration> bootstrap) {
+    }
+
+    @Override
+    public void run(Configuration config, Environment environment) {
+        Conchordance conchordance = new Conchordance();
+
+        // resources
+        environment.jersey().register(new ChordResource(conchordance));
+        environment.jersey().register(new FretboardResource(conchordance));
+        environment.jersey().register(new InstrumentResource());
+
+        // CORS support
+        FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter("allowedOrigins", "*");
+        filter.setInitParameter("allowedHeaders", "Authorization,Content-Type,X-Api-Key,Accept,Origin");
+        filter.setInitParameter("allowedMethods", "GET,POST,PUT,DELETE,OPTIONS");
+        filter.setInitParameter("preflightMaxAge", "5184000"); // 2 months
+        filter.setInitParameter("allowCredentials", "true");
+    }
+}
---
+++ src/main/java/com/conchordance/api/resources/FretboardResource.java
@@ -0,0 +1,42 @@
+package main.java.com.conchordance.api.resources;
+
+import com.conchordance.Conchordance;
+import com.conchordance.instrument.FretboardModel;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.LinkedList;
+import java.util.List;
+
+@Path("/fretboards")
+@Produces(MediaType.APPLICATION_JSON)
+public class FretboardResource {
+    private final Conchordance conchordance;
+
+    public FretboardResource(Conchordance conchordance) {
+        this.conchordance = conchordance;
+    }
+
+    @GET
+    public List<List<Boolean>> getFretboardModels(
+            @QueryParam("instrument") String instrument,
+            @QueryParam("type") String chordType,
+            @QueryParam("root") String root) {
+        FretboardModel model = conchordance.getFretboard(instrument, root, chordType);
+        List<List<Boolean>> stringArray = new LinkedList<>();
+
+        for (int s = 0; s < model.getInstrument().strings; s++) {
+            List<Boolean> fretArray = new LinkedList<>();
+            for (int f = 0; f < model.getInstrument().frets; f++) {
+                fretArray.add(model.hasNoteAt(s, f));
+            }
+            stringArray.add(fretArray);
+        }
+
+        return stringArray;
+    }
+    
+    @OPTIONS
+    public void options() {
+    }
+}
