97812b333a3ad930f83a3baa268736e2992daca2
jcbauschatz <jcbauschatz@revolutionehr.com>
2016-06-06 16:09:48 -0500

    Added Ukelele

--- src/main/java/com/conchordance/fretted/InstrumentBank.java
+++ src/main/java/com/conchordance/fretted/InstrumentBank.java
@@ -22,22 +22,25 @@ public class InstrumentBank {
 		DEFAULT_BANK.addInstrument(Instrument.GUITAR);
 
         DEFAULT_BANK.addInstrument(new Instrument("Guitar - DADGAD", 14, Clef.TREBLE,
-                new Note[] {new Note(D, 0, 5), new Note(A, 0, 4), new Note(G, 0, 4), new Note(D, 0, 4), new Note(A, 0, 3), new Note(D, 0, 3)}));
+                new Note(D, 0, 5), new Note(A, 0, 4), new Note(G, 0, 4), new Note(D, 0, 4), new Note(A, 0, 3), new Note(D, 0, 3)));
 
         DEFAULT_BANK.addInstrument(new Instrument("Guitar - DADF#AD", 14, Clef.TREBLE,
-                new Note[] {new Note(D, 0, 5), new Note(A, 0, 4), new Note(F, 1, 4), new Note(D, 0, 4), new Note(A, 0, 3), new Note(D, 0, 3)}));
+                new Note(D, 0, 5), new Note(A, 0, 4), new Note(F, 1, 4), new Note(D, 0, 4), new Note(A, 0, 3), new Note(D, 0, 3)));
 
 		DEFAULT_BANK.addInstrument(new Instrument("Guitar - Drop D", 14, Clef.TREBLE,
-				new Note[] {new Note(E, 0, 5), new Note(B, 0, 4), new Note(G, 0, 4), new Note(D, 0, 4), new Note(A, 0, 3), new Note(D, 0, 3)}));
+				new Note(E, 0, 5), new Note(B, 0, 4), new Note(G, 0, 4), new Note(D, 0, 4), new Note(A, 0, 3), new Note(D, 0, 3)));
 
         DEFAULT_BANK.addInstrument(new Instrument("Bass Guitar", 14, Clef.BASS,
-                new Note[] {new Note(G, 0, 2), new Note(D, 0, 2), new Note(A, 0, 1), new Note(E, 0, 1)}));
+                new Note(G, 0, 2), new Note(D, 0, 2), new Note(A, 0, 1), new Note(E, 0, 1)));
 
 		DEFAULT_BANK.addInstrument(new Instrument("Irish Bouzouki", 12, Clef.TREBLE,
-				new Note[] {new Note(D, 0, 5), new Note(A, 0, 4), new Note(D, 0, 4), new Note(G, 0, 3)}));
+				new Note(D, 0, 5), new Note(A, 0, 4), new Note(D, 0, 4), new Note(G, 0, 3)));
 
 		DEFAULT_BANK.addInstrument(new Instrument("Mandolin", 12, Clef.TREBLE,
-				new Note[] {new Note(E, 0, 5), new Note(A, 0, 4), new Note(D, 0, 4), new Note(G, 0, 3)}));
+				new Note(E, 0, 5), new Note(A, 0, 4), new Note(D, 0, 4), new Note(G, 0, 3)));
+
+		DEFAULT_BANK.addInstrument(new Instrument("Ukelele (GCEA)", 12, Clef.TREBLE,
+				new Note(A, 0, 5), new Note(E, 0, 5), new Note(C, 0, 5), new Note(G, 0, 5)));
 
 		DEFAULT_BANK.addInstrument(Instrument.BANJO);
 	}
